86ef80b76489364fb146f7ea6de4bc616d0f4c9c
Courtney Wicomb <ST10051000@vcconnect.edu.za>
2024-10-01 00:17:00 +0200

    final push

---
+++ OPSC-7314-master/app/src/main/java/com/example/tseaafricaapp/LoginPage.kt
@@ -0,0 +1,157 @@
+package com.example.tseaafricaapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.regex.Pattern
+
+class LoginPage : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 9001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val gmailBtn = findViewById<Button>(R.id.gmailBtn)
+
+        gmailBtn.setOnClickListener {
+            signIn()
+        }
+        val emailTxt = findViewById<EditText>(R.id.emailTxt)
+        val passwordTxt = findViewById<TextInputEditText>(R.id.passwordTxt)
+        val loginBtn = findViewById<Button>(R.id.createAccBtn)
+        val linkTxt3 = findViewById<TextView>(R.id.linktxt3)
+
+        loginBtn.setOnClickListener {
+            val email = emailTxt.text.toString()
+            val password = passwordTxt.text.toString()
+            if (validateEmail(email) && password.isNotEmpty()) {
+                //val intent = Intent(this, Home::class.java)
+                //startActivity(intent)
+                signInWithEmailPassword(email, password)
+            }
+        }
+
+        linkTxt3.setOnClickListener {
+            // Navigate to the Signup page
+            val intent = Intent(this, RegisterPage::class.java)  // Make sure to replace with your actual signup activity class
+            startActivity(intent)
+        }
+    }
+
+    private fun signInWithEmailPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    checkUserExists(user)
+                } else {
+                    Toast.makeText(this, "Authentication failed. Account does not exist.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun checkUserExists(user: FirebaseUser?) {
+        if (user != null) {
+            // User exists, proceed to Home activity
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            // User does not exist
+            Toast.makeText(this, "User account does not exist. Please sign up.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    checkUserExists(user)
+                    // User is signed in, navigate to the next activity
+                    //val intent = Intent(this, Home::class.java)
+                    //startActivity(intent)
+                    //finish()
+                } else {
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+    private fun validateEmail(email: String): Boolean {
+        return if (Pattern.compile(
+                "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.com$"
+            ).matcher(email).matches()) {
+            true
+        } else {
+            Toast.makeText(this, "Email must be in the format example@example.com", Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        val passwordPattern = Pattern.compile(
+            "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{12,}$"
+        )
+        return if (passwordPattern.matcher(password).matches()) {
+            true
+        } else {
+            Toast.makeText(
+                this,
+                "Password must be at least 12 characters long, contain uppercase and lowercase letters, a number, and a special character",
+                Toast.LENGTH_LONG
+            ).show()
+            false
+        }
+    }
+}
---
+++ OPSC-7314-master/app/src/main/java/com/example/tseaafricaapp/RegisterPage.kt
@@ -0,0 +1,243 @@
+package com.example.tseaafricaapp
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
+import java.util.regex.Pattern
+
+class RegisterPage : AppCompatActivity() {
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            // The user is already signed in, navigate to Home
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish() // finish the current activity to prevent the user from coming back to the SignInActivity using the back button
+        }
+
+
+        val signInButton = findViewById<Button>(R.id.gmailBtn)
+        signInButton.setOnClickListener {
+            signIn()
+        }
+
+        // Enable Edge-to-Edge
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Initialize UI elements
+        val nameTxt = findViewById<EditText>(R.id.nameTxt)
+        val emailTxt = findViewById<EditText>(R.id.emailTxt)
+        val passwordTxt = findViewById<TextInputEditText>(R.id.passwordTxt)
+        val createAccBtn = findViewById<Button>(R.id.createAccBtn)
+        val checkBox = findViewById<CheckBox>(R.id.checkBox)
+        val linktxt2 = findViewById<TextView>(R.id.linktxt2)
+
+        createAccBtn.setOnClickListener {
+            // Retrieve user inputs
+            val fullName = nameTxt.text.toString()
+            val email = emailTxt.text.toString()
+            val password = passwordTxt.text.toString()
+
+            // Validate inputs
+            if (validateFullName(fullName) && validateEmail(email) && password.isNotEmpty()) {
+                if (checkBox.isChecked) {
+                    // Proceed to register the user using Firebase Authentication
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful){
+                                Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT).show()
+                                val user = auth.currentUser
+
+//-----------------------------add UID into realtime database
+                                val userId = user?.uid
+                                val userEmail = user?.email
+                                val database = FirebaseDatabase.getInstance().reference
+                                val userData = hashMapOf(
+                                    "email" to userEmail
+                                )as HashMap<String, Any>
+
+                                database.child("users").child(userId!!).updateChildren(userData)
+                                    .addOnCompleteListener { databaseTask ->
+                                        if (databaseTask.isSuccessful) {
+                                            Toast.makeText(this, "User ID saved to database!", Toast.LENGTH_SHORT).show()
+                                        } else {
+                                            Toast.makeText(this, "Failed to save user ID to database: ${databaseTask.exception?.message}", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+
+//-----------------------------END: add UID into realtime database
+                                // Navigate to the next activity
+                                val intent = Intent(this, Home::class.java)
+                                startActivity(intent)
+                            }
+                            else {
+                                Toast.makeText(this, "Registration failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    val intent = Intent(this, Home::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Please agree to TseaAfrica's Terms and Conditions", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        linktxt2.setOnClickListener {
+            // Navigate to Login page
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Registration successful, navigate to PersonalisedMeals
+                    Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, Home::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If registration fails, show a message to the user
+                    Toast.makeText(this, "Registration failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+    private fun signIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google sign in failed: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(this, "Signed in as ${user?.displayName}", Toast.LENGTH_SHORT).show()
+
+//--------------add userID and email into realtime database
+                    val userId = user?.uid
+                    val userEmail = user?.email
+                    val database = FirebaseDatabase.getInstance().reference
+                    val userData = hashMapOf(
+                        "email" to userEmail
+                    ) as HashMap<String, Any>
+                    database.child("users").child(userId!!).updateChildren(userData)
+                        .addOnCompleteListener { databaseTask ->
+                            if (databaseTask.isSuccessful) {
+                                Toast.makeText(this, "User ID saved to database!", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this, "Failed to save user ID to database: ${databaseTask.exception?.message}", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+//--------------END: add userID and email into realtime database
+                    startActivity(Intent(this, Home::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+
+
+    private fun validateFullName(fullName: String): Boolean {
+        return if (fullName.isNotEmpty() && fullName.all { it.isLetter() || it.isWhitespace() }) {
+            true
+        } else {
+            Toast.makeText(this, "Full name must contain only letters and spaces", Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return if (Pattern.compile(
+                "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.com$"
+            ).matcher(email).matches()) {
+            true
+        } else {
+            Toast.makeText(this, "Email must be in the format example@example.com", Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        val passwordPattern = Pattern.compile(
+            "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{12,}$"
+        )
+        return if (passwordPattern.matcher(password).matches()) {
+            true
+        } else {
+            Toast.makeText(
+                this,
+                "Password must be at least 12 characters long, contain uppercase and lowercase letters, a number, and a special character",
+                Toast.LENGTH_LONG
+            ).show()
+            false
+        }
+    }
+}
